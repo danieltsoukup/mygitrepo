65be3b6c0849ca140fdac4aa634381af49ba0236
Daniel Soukup <dsoukup@brainststaion.io>
2020-11-04 16:43:07 +0100

    update to hello message and comments

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,6 @@
 def hello():
-	print("Hello world")
+	print("Hello, world!")
 
-# this statement helps with running hello() whenever this
-# file is ran from the command line
 if __name__ == "__main__":
 	hello()
 
